5135ca18d298da6cfcb50af53f1c3a5a4c177d85
Joseph Sams <joe.mac@Josephs-iMac.local>
2017-11-17 00:28:37 -0500

    configure Docker and Nginx config

--- src/api.js
+++ src/api.js
@@ -4,17 +4,19 @@ export default {
   logs: {
     upload: formData =>
       axios
-        .post("http://localhost:5000/api/logs/upload", formData)
+        .post("http://104.131.164.48:5000/api/logs/upload", formData)
         .then(res => res.data),
     parse: fileName =>
       axios
-        .get("http://localhost:5000/api/logs/parse", {
+        .get("http://104.131.164.48:5000/api/logs/parse", {
           params: { fileName: fileName }
         })
         .then(res => res.data),
     getAllLogs: () =>
-      axios.get("http://localhost:5000/api/logs/").then(res => res.data),
+      axios.get("http://104.131.164.48:5000/api/logs/").then(res => res.data),
     getLogByID: id =>
-      axios.get(`http://localhost:5000/api/logs/${id}`).then(res => res.data)
+      axios
+        .get(`http://104.131.164.48:5000/api/logs/${id}`)
+        .then(res => res.data)
   }
 };
